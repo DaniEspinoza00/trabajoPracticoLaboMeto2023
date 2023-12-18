c52ecb0e2f652c82bf15b8b1695b5a26acee0f79
luciano ruffinengo <lucianoruffinengogmail.com>
2023-12-18 15:58:50 -0300

    compra select

--- src/app/components/compra/compra.component.ts
+++ src/app/components/compra/compra.component.ts
@@ -17,7 +17,19 @@ export class CompraComponent implements OnInit {
   listaItemsCarrito: ItemCarrito[] = []
   user: Usuario | undefined
   // stock: LibroStock | undefined
+  selectedOption: string = '';
+  tarjeta: number = 0;
 
+  onChangeSelect() {
+    switch (this.selectedOption) {
+      case 'num':
+        this.tarjeta = +this.selectedOption
+        break;
+      default:
+        this.tarjeta = 0
+        break;
+    }
+  }
 
 
   constructor(private router: Router,
@@ -50,46 +62,49 @@ export class CompraComponent implements OnInit {
     let i = 0
     let stockLibro = 0
     if (this.user?.tarjetaCredito.numeroTarjeta != 0) {
-
-      this.LibrosStockService.getStockHttp().subscribe({
-        next: (stock) => {
-          let j = 0
-          for (let i = 0; i < stock.length; i++) {
-            if (j < this.listaItemsCarrito.length) {
-              if (stock[i].id === this.listaItemsCarrito[j].id) {
-                if (this.listaItemsCarrito[j].cantidad > stock[i].stock) {
-                  stockLibro = stock[i].stock
-                  flag = 0
-                }
-                j++;
-              }
-
-            }
-          }
-          if (flag === 1) {
-            j = 0
-            console.log(`j = ${j}`);
+      if(this.tarjeta != 0){
+        this.LibrosStockService.getStockHttp().subscribe({
+          next: (stock) => {
+            let j = 0
             for (let i = 0; i < stock.length; i++) {
               if (j < this.listaItemsCarrito.length) {
                 if (stock[i].id === this.listaItemsCarrito[j].id) {
-                  stock[i].stock = stock[i].stock - this.listaItemsCarrito[j].cantidad
-                  this.LibrosStockService.putLibroHttp(stock[i]).subscribe()
-                  j++
+                  if (this.listaItemsCarrito[j].cantidad > stock[i].stock) {
+                    stockLibro = stock[i].stock
+                    flag = 0
+                  }
+                  j++;
                 }
+  
               }
             }
-            this.agregarHistorial()
-            localStorage.removeItem('carrito')
-            this.listaItemsCarrito = []
-            this.router.navigate(['felicidades'])
-          } else {
-            alert(`No hay stock suficiente para el producto: "${this.listaItemsCarrito[i].titulo}", solo hay: "${stockLibro}" libros en stock del producto mencionado`)
+            if (flag === 1) {
+              j = 0
+              for (let i = 0; i < stock.length; i++) {
+                if (j < this.listaItemsCarrito.length) {
+                  if (stock[i].id === this.listaItemsCarrito[j].id) {
+                    stock[i].stock = stock[i].stock - this.listaItemsCarrito[j].cantidad
+                    this.LibrosStockService.putLibroHttp(stock[i]).subscribe()
+                    j++
+                  }
+                }
+              }
+              this.agregarHistorial()
+              localStorage.removeItem('carrito')
+              this.listaItemsCarrito = []
+              this.router.navigate(['felicidades'])
+            } else {
+              alert(`No hay stock suficiente para el producto: "${this.listaItemsCarrito[i].titulo}", solo hay: "${stockLibro}" libros en stock del producto mencionado`)
+            }
+          },
+          error: (error) => {
+            console.log('No se pudo acceder al stock de los libros', error);
           }
-        },
-        error: (error) => {
-          console.log('No se pudo acceder al stock de los libros', error);
-        }
-      })
+        })
+      }else{
+        alert("Por favor seleccione una tarjeta")
+      }
+
     }
     else {
       alert("El usuario no tiene ninguna tarjeta agregada")
